35ce9bb8e6b3bc45d4eb1fe99b87d1baeee1b8fc
phil <smartp@cf.ac.uk>
2017-03-14 11:12:09 +0000

    added HttpRequest to login

--- src/main/java/uk/ac/cardiff/nsa/security/controller/LoginController.java
+++ src/main/java/uk/ac/cardiff/nsa/security/controller/LoginController.java
@@ -2,9 +2,12 @@ package uk.ac.cardiff.nsa.security.controller;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.http.HttpRequest;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
+import javax.servlet.http.HttpServletRequest;
 
 /**
  * Created by philsmart on 13/03/2017.
@@ -15,8 +18,14 @@ public class LoginController {
     private static final Logger log = LoggerFactory.getLogger(LoginController.class);
 
     @RequestMapping(value = "/login", method= RequestMethod.GET, produces ="application/json" )
-    public String login(){
+    public String login(HttpServletRequest request){
+
         log.info("Login request");
+
+        Object auth = request.getHeader("Authorization");
+
+        log.debug("Has auth header [{}]",auth);
+
         return  "token";
 
 
