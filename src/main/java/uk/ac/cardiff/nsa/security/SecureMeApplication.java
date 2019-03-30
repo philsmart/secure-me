ee875ce8650dc330868117a86c9d8f241f3d3ade
Phil Smart <smartp@cf.ac.uk>
2019-03-30 21:39:34 +0000

    add jsf bean

--- src/main/java/uk/ac/cardiff/nsa/security/SecureMeApplication.java
+++ src/main/java/uk/ac/cardiff/nsa/security/SecureMeApplication.java
@@ -1,23 +1,24 @@
+
 package uk.ac.cardiff.nsa.security;
+
+import javax.faces.webapp.FacesServlet;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.ServletRegistrationBean;
 import org.springframework.context.annotation.Bean;
 
-import javax.faces.webapp.FacesServlet;
-
 @SpringBootApplication
 public class SecureMeApplication {
 
-    public static void main(String[] args) {
+    public static void main(final String[] args) {
         SpringApplication.run(SecureMeApplication.class, args);
     }
 
-
     /**
-     * Register the {@link FacesServlet} within the current {@link org.springframework.boot.web.servlet.ServletContextInitializer},
-     * and hence with the v3 {@link javax.servlet.Servlet} provider. This maps all URL patterns *.xhtml to the {@link FacesServlet}.
+     * Register the {@link FacesServlet} within the current
+     * {@link org.springframework.boot.web.servlet.ServletContextInitializer}, and hence with the v3
+     * {@link javax.servlet.Servlet} provider. This maps all URL patterns *.xhtml to the {@link FacesServlet}.
      *
      * @return a {@link ServletRegistrationBean} with registered {@link FacesServlet}
      */
@@ -25,12 +26,17 @@ public class SecureMeApplication {
     public ServletRegistrationBean servletRegistrationBean() {
         final FacesServlet servlet = new FacesServlet();
 
-        final ServletRegistrationBean servletRegistrationBean = new ServletRegistrationBean(servlet, new String[]{"*.xhtml"});
+        final ServletRegistrationBean servletRegistrationBean =
+                new ServletRegistrationBean(servlet, new String[] {"*.xhtml"});
         servletRegistrationBean.setName("FacesServlet");
         servletRegistrationBean.setLoadOnStartup(1);
 
         return servletRegistrationBean;
     }
 
+    @Bean
+    public com.sun.faces.config.ConfigureListener mojarraConfigureListener() {
+        return new com.sun.faces.config.ConfigureListener();
+    }
 
 }
